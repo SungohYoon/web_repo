abdfb38c2c715ec6660ac8c8e1a6dd95692043ad
nsa30 <nsa30@DESKTOP-KAQA194>
2023-10-26 01:46:37 +0900

    달력

--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -1,4 +1,18 @@
 // calendar.js
-function makeCalendar(){
-    
+const dayOfTheWeek = ["일", "월", "화", "수", "목", "금", "토"];
+function Calendar() {
+  let html = "";
+  function makeHead() {
+    let head = "";
+    head += "<table border=1>";
+    head += "<thead>";
+    head += "<tr>";
+    for (let i = 0; i < dayOfTheWeek.length; i++) {
+      head += "<th>" + dayOfTheWeek.length[i] + "</th>";
+    }
+    head += "</tr>";
+    head += "</thead>";
+  }
+
+  function makeBody() {}
 }
